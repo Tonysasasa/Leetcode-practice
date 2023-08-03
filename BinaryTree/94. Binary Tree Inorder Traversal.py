c22202a6a9794ca587b7d8cfd2b3d97c08ea000c
Tony_Xia <tonyxia1995@outlook.com>
2023-08-03 16:11:48 +0800

    stack - binaryTree traverse

--- BinaryTree/94. Binary Tree Inorder Traversal.py
+++ BinaryTree/94. Binary Tree Inorder Traversal.py
@@ -1,4 +1,4 @@
-# Array - Easy
+# Tree - Easy
 # https://leetcode.com/problems/binary-tree-inorder-traversal/
 
 # Definition for a binary tree node.
@@ -13,14 +13,28 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[int]
         """
-        l = []
-        def inorder(root,l):
-            if root: 
-                inorder(root.left,l)
-                l.append(root.val)
-                inorder(root.right,l)           
-        inorder(root,l)
-        return l
+        # l = []
+        # def inorder(root,l):
+        #     if root: 
+        #         inorder(root.left,l)
+        #         l.append(root.val)
+        #         inorder(root.right,l)           
+        # inorder(root,l)
+        # return l
+
+        if not root: return []
+        stack = []; res = []; cur = root
+        while stack or cur:
+            if cur:
+                stack.append(cur)
+                cur = cur.left
+            else:               
+                cur = stack.pop()
+                res.append(cur.val)
+                cur = cur.right
+        return res
     
 # Main idea: Recursion - Continously traverse the binary tree from left to right. Defining the recursion parameters, 
 # ending condition and traverse logic.
+# - Stack - Note: idea is different with Preorder search. It starts with continously traversing to the left-most element,
+# then save the value and pop the stack to go to the right branch. 
