1732b4ec7086125d191362da6152d8750ce0ee07
Tony_Xia <tonyxia1995@outlook.com>
2023-08-03 15:51:52 +0800

    stack - BTree traverse

--- BinaryTree/144. Binary Tree Preorder Traversal.py
+++ BinaryTree/144. Binary Tree Preorder Traversal.py
@@ -13,14 +13,28 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[int]
         """
-        l = []
-        def preorder(root,l):
-            if root:
-                l.append(root.val)
-                preorder(root.left,l)
-                preorder(root.right,l)
-        preorder(root,l)
-        return l
+        # l = []
+        # def preorder(root,l):
+        #     if root:
+        #         l.append(root.val)
+        #         preorder(root.left,l)
+        #         preorder(root.right,l)
+        # preorder(root,l)
+        # return l
+
+        if root is None: return []
+        stack = []; res = []
+        stack.append(root)
+        while stack:
+            node = stack.pop()
+            res.append(node.val)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        return res
     
 # Main idea: Recursion - Continously traverse the binary tree from left to right after getting the root.val. 
 # Defining the recursion parameters, ending condition and traverse logic.
+# - Stack - use a stack to replace recursion algorithm. Store the current root if it is not none. Save the 
+# root.val into a res array, and then point to the right and left element. Need to store right first then left. 
