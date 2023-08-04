a9a94a554c238b32a8a594e59d1bd54df5d2cb27
Tony_Xia <tonyxia1995@outlook.com>
2023-08-04 16:55:08 +0800

    stack - binaryTree traverse

--- BinaryTree/145. Binary Tree Postorder Traversal.py
+++ BinaryTree/145. Binary Tree Postorder Traversal.py
@@ -13,14 +13,26 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[int]
         """
-        l = []
-        def postorder(root,l):
-            if root:
-                postorder(root.left,l)
-                postorder(root.right,l)
-                l.append(root.val)
-        postorder(root,l)
-        return l
+        # l = []
+        # def postorder(root,l):
+        #     if root:
+        #         postorder(root.left,l)
+        #         postorder(root.right,l)
+        #         l.append(root.val)
+        # postorder(root,l)
+        # return l
+        if not root: return []
+        res = []; stack = [root]
+        while stack:
+            cur = stack.pop()
+            res.append(cur.val)
+            if cur.left:
+                stack.append(cur.left)
+            if cur.right:
+                stack.append(cur.right)
+        return res[::-1]
     
 # Main idea: Recursion - Continously traverse the binary tree from left to right before getting the root.val. 
 # Defining the recursion parameters, ending condition and traverse logic.
+# - stack - Same idea with Preorder traversal byusing stack method. Needs to change the order from
+# mid-left-right to mid-right-left then inverse the order to left-right-mid
